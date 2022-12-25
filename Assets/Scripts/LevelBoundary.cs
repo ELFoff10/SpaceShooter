ec0c81c5c69ac30912b68f31900a0aeebd6620b1
ELFoff10 <andrei.kaliadzin@gmail.com>
2022-12-25 23:57:17 +0300

    25.12.22 23-55

---
+++ Assets/Scripts/LevelBoundary.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    public class LevelBoundary : SingletonBase<LevelBoundary>
+    {
+        [SerializeField] private float m_WorldRadius;
+        public float Radius => m_WorldRadius;
+
+        public enum Mode
+        {
+            Limit, 
+            Teleport
+        }
+
+        [SerializeField] private Mode m_LimitMode;
+        public Mode LimitMode => m_LimitMode;
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            UnityEditor.Handles.color = Color.green;
+            UnityEditor.Handles.DrawWireDisc(transform.position, transform.forward, m_WorldRadius);
+        }
+#endif
+    }
+}
+
