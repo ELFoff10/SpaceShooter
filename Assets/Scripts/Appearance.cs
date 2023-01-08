2156c91f0d61dd10dd86de9615d4cae54d3e98c9
ELFoff10 <andrei.kaliadzin@gmail.com>
2023-01-08 13:23:12 +0300

    08.01.2023 13-25

--- Assets/Scripts/Appearance.cs
+++ Assets/Scripts/Appearance.cs
@@ -1,41 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Appearance : MonoBehaviour
 {
-    [SerializeField] private float m_TimeBeforeShutdown, m_TimeAfterShutdown;
+    [SerializeField] private float m_Time;
+
+    private float m_Timer;
 
     [SerializeField] private SpriteRenderer m_SpriteRenderer;
 
-    private bool m_Running = true;
-
     private void Update()
     {
-        var tbs = m_TimeBeforeShutdown;
-        var tas = m_TimeAfterShutdown;
+        m_Timer += Time.deltaTime / m_Time;
 
-        m_TimeBeforeShutdown -= Time.deltaTime;
-
-        if (m_TimeBeforeShutdown <= 0)
-        {
-            m_Running = false;
-            m_TimeBeforeShutdown = tbs;
-        }
-
-        if (m_Running)
-        {
-            m_SpriteRenderer.GetComponent<SpriteRenderer>().enabled = true;
-            m_SpriteRenderer.GetComponent<BoxCollider2D>().enabled = true;
-            m_TimeBeforeShutdown = tbs;
-        }
-
-
-        if (m_Running == false)
+        if ((int) m_Timer % 2 == 0)
         {
             m_SpriteRenderer.GetComponent<SpriteRenderer>().enabled = false;
             m_SpriteRenderer.GetComponent<BoxCollider2D>().enabled = false;
 
+        }
+        if ((int) m_Timer % 2 == 1)
+        {
+            m_SpriteRenderer.GetComponent<SpriteRenderer>().enabled = true;
+            m_SpriteRenderer.GetComponent<BoxCollider2D>().enabled = true;
         }
     }
 }
